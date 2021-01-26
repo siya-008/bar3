4121a79b18d15d85641c507d0a4cd702d8c41fbf
siya-008 <siyavanshika@gmail.com>
2021-01-26 11:47:44 +0530

    messege

--- sketch.js
+++ sketch.js
@@ -1,9 +1,40 @@
-function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+var balloon, background;
+function preload() {
+  backgroundImg = loadImage("images/1.png")
+  balloonImage = loadAnimation("images/2.png", "images/3.png", "images/4.png");
 }
 
-function draw() {
-  background(255,255,255);  
-  drawSprites();
+function setup(){
+   database = firebase.database();
+   console.log(database);
+    createCanvas(500,500);
+
+balloon = createSprite (100,400, 20, 20)
+  balloon.addAnimation("balloon", balloonImage);
+  balloon.scale = 0.4;
+
+
 }
+
+function draw(){
+
+    background(backgroundImg);
+  
+        if(keyDown(LEFT_ARROW)){
+            balloon.x = balloon.x - 10;
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            balloon.x = balloon.x + 10;
+        }
+        else if(keyDown(UP_ARROW)){
+            balloon.y = balloon.y - 10;
+        }
+        else if(keyDown(DOWN_ARROW)){
+             balloon.y = balloon.y + 10;
+        }
+        drawSprites();
+    }
+
+    
+
+
